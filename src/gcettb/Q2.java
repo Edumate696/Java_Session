985442ac9940e95534c55b9baa3ef24da2ddcab7
Arpan Mahanty <edumate696@gmail.com>
2021-04-29 12:55:26 +0530

    Updated

--- src/gcettb/Q2.java
+++ src/gcettb/Q2.java
@@ -6,9 +6,6 @@ package gcettb;
  */
 
 public class Q2 {
-    public static void main(String[] args) {
-        Q2 q2 = new Q2(n);
-    }
 
     static int a = 10;
     static int n;
@@ -29,5 +26,10 @@ public class Q2 {
     static
     {
         a = 60;
+    }
+
+
+    public static void main(String[] args) {
+        Q2 q2 = new Q2(n);
     }
 }
---
+++ src/gcettb/Q4.java
@@ -0,0 +1,19 @@
+package gcettb;
+
+/**
+ * Question: <BR>
+ *      What will be the output of this script or the main method declared in this class ?? <BR>
+ *      Also describe all possible error in this code, if any ( both Ethical and Syntactic )
+ */
+
+public class Q4 {
+
+    public void Q4(){
+        System.out.println("Hello World!!!");
+    }
+
+    public static void main(String[] args) {
+        Q4 q4 = new Q4();
+    }
+
+}
